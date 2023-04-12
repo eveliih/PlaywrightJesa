e573faede821490c60e95690255cea349451fb00
Sebaswo <sebastwo@metropolia.fi>
2023-04-12 15:01:00 +0300

    firefox pois, luotunnus sposti -testit

--- tests/loytyykoJakso.spec.ts
+++ tests/loytyykoJakso.spec.ts
@@ -3,7 +3,7 @@ test.describe("Episode Detail test", () => {
 
     test("kummeli has name", async ({ page }) => {
       await page.goto("https://areena.yle.fi/1-3339547");
-      await page.getByRole("button", { name: "Kausi 3", exact: true }).click();
+      await page.getByRole("button", { name: "Kausi 3", exact: true }).click({force: true});
       const expected = "5. Kummeli"
       const name = page.getByText(expected);
       expect(name).not.toBeNull();
@@ -11,7 +11,7 @@ test.describe("Episode Detail test", () => {
 
     test("kummeli has date", async ({ page }) => {
         await page.goto("https://areena.yle.fi/1-3339547");
-        await page.getByRole("button", { name: "Kausi 3", exact: true }).click();
+        await page.getByRole("button", { name: "Kausi 3", exact: true }).click({force: true});
         const expected = "ti 8.3.2016"
         const name = page.getByText(expected);
         expect(name).not.toBeNull();
--- tests/luoTunnus.spec.ts
+++ tests/luoTunnus.spec.ts
@@ -3,11 +3,30 @@ import { test, expect } from "@playwright/test";
 test.describe("Log in tests", () => {
   test("one", async ({ page }) => {
     await page.goto("https://areena.yle.fi");
-    await page.getByRole("button", {name: "Kirjaudu", exact: true}).click();
-    await page.waitForSelector('input[id="email"]');
-    await page.getByRole("button", {name: "Luo Yle Tunnus", exact: true}).click();
-    const email = await page.$('input[autocomplete="username]"').fill("fake.email@asdf");
-    await email.fill("fake.email@asdf");
+    await page.getByRole('button', { name: 'Kirjaudu', exact: true }).click({force: true});
+    const popup = page.frameLocator('.tunnus-sdk__iframe');
+    await popup.getByText("Luo Yle Tunnus").click({force:true});
+    await popup.locator("#email").fill("fake.email@asdf");
+    const error = await page.getByText("Tarkista sähköpostiosoitteen muoto.");
+    expect(error).not.toBeNull();
+  });
+
+  test("two", async ({ page }) => {
+    await page.goto("https://areena.yle.fi");
+    await page.getByRole('button', { name: 'Kirjaudu', exact: true }).click({force: true});
+    const popup = page.frameLocator('.tunnus-sdk__iframe');
+    await popup.getByText("Luo Yle Tunnus").click({force:true});
+    await popup.locator("#email").fill("väääääärä");
+    const error = await page.getByText("Tarkista sähköpostiosoitteen muoto.");
+    expect(error).not.toBeNull();
+  });
+
+  test("three", async ({ page }) => {
+    await page.goto("https://areena.yle.fi");
+    await page.getByRole('button', { name: 'Kirjaudu', exact: true }).click({force: true});
+    const popup = page.frameLocator('.tunnus-sdk__iframe');
+    await popup.getByText("Luo Yle Tunnus").click({force:true});
+    await popup.locator("#email").fill("fake.email£gmail.com");
     const error = await page.getByText("Tarkista sähköpostiosoitteen muoto.");
     expect(error).not.toBeNull();
   });
